79d638bb81ffb7da91693e07f7207cad9dbba526
bradsharp <bradleysharpie@gmail.com>
2019-06-15 22:32:52 +0100

    Added test files

--- formatter.js
+++ formatter.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 let input = require("./api.json");
 
 function write(name, data) {
-	fs.writeFile(`${name}.json`, JSON.stringify(data), 'utf8', (err) => {
+	fs.writeFile(`${name}.json`, JSON.stringify(data, null, "	"), 'utf8', (err) => {
 		if (err)
 			console.error(err);
 	});
@@ -13,16 +13,26 @@ function write(name, data) {
 let instances = {};
 let classes = input.Classes;
 
+var reads = 0;
+
 classes.forEach(instance => {
-	instances[instance.Name] = {
-		Members: instance.Members,
-		Super: instance.Superclass,
-		Tags: instance.Tags,
-		ShortDescription: "Undocumented",
-		Description: "",
-		Related: [],
-	}
+	let name = instance.Name
+	reads++;
+	fs.readFile(`Instances/${name}/Description.md`, "utf8", (err, data) => {
+		console.log(name, err, data)
+		reads--;
+		instances[name] = {
+			// Members: instance.Members,
+			Super: instance.Superclass,
+			Tags: instance.Tags,
+			ShortDescription: "Undocumented",
+			Description: data == null ? "" : data,
+			Related: [],
+		}
+		if (reads == 0)
+			write("instances", instances);
+	});
+	
 });
 
 write("enums", input.Enums);
-write("instances", instances);
